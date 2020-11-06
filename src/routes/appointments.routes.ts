00b72fcafaec4d44ef918eff193ee70787c9e1f5
Ian Gerolamo <iangerolamo@gmail.com>
2020-11-05 21:40:04 -0300

    First commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+// http://localhost:3333/appointments;
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  }
+
+  appointments.push(appointment);
+
+  return response.json(appointment)
+
+});
+
+export default appointmentsRouter;
